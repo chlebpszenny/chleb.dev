9a77594be655585e97cae1ba0575ef1077e94a03
chleb <162454828+chlebpszenny@users.noreply.github.com>
2025-07-11 15:50:45 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>chleb</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>chleb.dev</h1>
+</header>
+<main>
+    <aside class="left">
+        <nav>
+            <ul>
+                <li><a href="index.php">main</a></li>
+                <li><a href="uno.php">uno</a></li>
+                <li><a href="dos.php">dos</a></li>
+                <li><a href="tres.php">tres</a></li>
+                <li><a href="quatro.php">quatro</a></li>
+                <li><a href="cinco.php">cinco</a></li>
+            </ul>
+        </nav>
+    </aside>
+    <section class="right">
