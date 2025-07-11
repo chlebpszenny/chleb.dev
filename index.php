a4b373870ec045c5d38d83f1e59c79c8a52d60e3
chleb <162454828+chlebpszenny@users.noreply.github.com>
2025-07-11 18:06:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chleb</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        chleb.dev
+    </header>
+    <div class="container">
+        <aside>
+            <ul>
+                <li><a href="index.php?id=a">a</a></li>
+                <li><a href="index.php?id=b">Tb</a></li>
+                <li><a href="index.php?id=c">c</a></li>
+                <li><a href="index.php?id=d">d</a></li>
+                <li><a href="index.php?id=e">e</a></li>
+                <li><a href="index.php?id=f">f</a></li><br>
+                <li><a href="index.php?id=g">g</a></li>
+            </ul>
+        </aside>
+        <main>
+            <?php
+                $id = isset($_GET['id']) ? $_GET['id'] : "";
 
-<?php include 'footer.php'; ?>
+                switch ($id)
+                {
+                    case 'a': require("a.php"); break;
+                    case 'b': require("b.php"); break;
+                    case 'c': require("c.php"); break;
+                    case 'd': require("d.php"); break;
+                    case 'e': require("e.php"); break;
+                    case 'f': require("f.php"); break;
+                    case 'g': require("g.php"); break;
+                    default: require("home.html");
+                }
+            ?>
+        </main>
+    </div>
+    <footer>
+        Copyright &copy; me ig
+    </footer>
+</body>
+</html>
